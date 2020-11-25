a18dbcfd9cba2cabd139a0dc719ff95268424220
ferruhcihan <ferruhcihan20@gmail.com>
2020-11-25 22:39:16 +0300

    Favorites updated

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,5 +1,3 @@
-import React, { useState, useEffect } from 'react';
-import axios from "axios"
 import { StyledFavorites, StyledImage } from "./Favorites.style"
 import Construction from "../../img/construction.gif"
 
