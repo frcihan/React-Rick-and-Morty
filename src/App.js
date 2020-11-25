3c214acf3ad8bc61f398481d385f59bf66e5c60e
ferruhcihan <ferruhcihan20@gmail.com>
2020-11-25 22:38:31 +0300

    App.js updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from "axios"
 import './App.css'
 import {Characters} from "./pages/Characters/index"
 import {Episodes} from "./pages/Episodes/index"
-import {Favorites} from "./pages/Favorites/index"
+// import {Favorites} from "./pages/Favorites/index"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import {Home} from "./pages/Home/index"
 import {Navbar} from "./components/Navbar"
@@ -18,7 +18,7 @@ const App = () => {
           <Route path="/details/:id" component={CharacterDetails} />
           <Route path="/characters" component={Characters} />
           <Route path="/episodes" component={Episodes} />
-          <Route path="/favorites" component={Favorites} />
+          {/* <Route path="/favorites" component={Favorites} /> */}
           <Route path="/" component={Home} />
         </Switch>
       </Router>
